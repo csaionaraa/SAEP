8f416b063fad4bfa9aa51c42dc2b09309c3212c0
Carla Saionara <109179480+csaionaraa@users.noreply.github.com>
2023-06-18 16:26:05 -0300

    start

--- api/server.js
+++ api/server.js
@@ -1,101 +1,109 @@
 import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
-
 const app = express();
-app.use(cors());
-const port = 3000;
+const PORT = 3000;
 
 // Configuração da conexão com o banco de dados
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'saep'
+  database: 'saep',
 });
 
-// Verificação da conexão com o banco de dados
+// Conectar-se ao banco de dados
 connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
-  console.log('Conexão com o banco de dados estabelecida com sucesso!');
-});
-
-// Rota para testar a conexão com o banco de dados
-app.get('/', (req, res) => {
-  res.send('Conexão com o banco de dados estabelecida com sucesso!');
-});
-
-// Rota para puxar patios
-// Rota para puxar patios
-app.get('/api/alocacao', (req, res) => {
-    const query = 'SELECT DISTINCT area, quantidade FROM alocacao ORDER BY area ASC';
-  
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Erro ao obter dados da tabela de alocação:', err);
-        res.status(500).json({ error: 'Erro ao obter dados da tabela de alocação' });
-      } else {
-        const setores = results.map(row => ({
-          area: row.area,
-          cor: row.quantidade > 0 ? 'azul' : 'branco',
-        }));
-  
-        res.json(setores);
-      }
-    });
+    if (err) {
+      console.error('Erro ao conectar-se ao banco de dados:', err);
+    } else {
+      console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    }
   });
-  
 
-//Rota para puxar os dados dos veiculos
+// Configuração do CORS
+app.use(cors());
+
+app.get('/api/alocacao', (req, res) => {
+  const query = 'SELECT area, quantidade FROM alocacao';
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Erro ao obter dados da tabela de alocação:', err);
+      res.status(500).json({ error: 'Erro ao obter dados da tabela de alocação' });
+    } else {
+      const setores = results.map(row => ({
+        area: row.area,
+        cor: row.quantidade > 0 ? 'azul' : 'branco',
+      }));
+
+      res.json(setores);
+    }
+  });
+});
+
 app.get('/api/automoveis/:alocacao', (req, res) => {
     const alocacao = req.params.alocacao;
     const query = `
-    SELECT automoveis.modelo, automoveis.preco, alocacao.quantidade
-    FROM automoveis
-    JOIN alocacao ON automoveis.id = alocacao.id
-    WHERE alocacao.area = ?
+      SELECT automoveis.id, automoveis.modelo, automoveis.preco, alocacao.quantidade
+      FROM automoveis
+      JOIN alocacao ON automoveis.id = alocacao.id
+      WHERE alocacao.area = ?
     `;
-
+  
     connection.query(query, [alocacao], (error, results) => {
-    if (error) {
+      if (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao consultar os veículos.' });
-    } else {
+      } else {
         res.json(results);
-    }
+      }
     });
-});
-//Rota para puxar os dados dos clientes
-app.get('/api/clientes', (req, res) => {
-    const query = 'SELECT * FROM clientes';
+  });
 
+  app.get('/api/clientes', (req, res) => {
+    const query = 'SELECT * FROM clientes';
+  
     connection.query(query, (error, results) => {
-    if (error) {
+      if (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao consultar os clientes.' });
-    } else {
+      } else {
         res.json(results);
-    }
+      }
     });
-});
-//Rota para puxar os dados da concessionaria
-app.get('/api/concessionaria', (req, res) => {
-    const query = 'SELECT * FROM concessionaria';
+  });
 
+  app.get('/api/concessionaria', (req, res) => {
+    const query = 'SELECT * FROM concessionaria';
+  
     connection.query(query, (error, results) => {
-    if (error) {
+      if (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao consultar a concessionaria.' });
-    } else {
+      } else {
         res.json(results);
-    }
+      }
     });
-});
+  });
 
+  app.put('/api/vendas/:automovelId', (req, res) => {
+    const automovelId = req.params.automovelId;
+  
+    const updateQuery = `UPDATE alocacao SET quantidade = quantidade - 1 WHERE automovel = ${automovelId}`;
+  
+    connection.query(updateQuery, (error, results) => {
+      if (error) {
+        console.error('Erro ao atualizar a quantidade do automóvel:', error);
+        res.status(500).json({ error: 'Erro ao atualizar a quantidade do automóvel' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Quantidade do automóvel atualizada com sucesso' });
+    });
+  });
+  
 // Inicialização do servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,9 @@
               <tr v-for="car in carData" :key="car.modelo">
                 <td>{{ car.modelo }}</td>
                 <td>{{ car.preco }}</td>
-                <td>{{ car.quantidade }}</td>
+                <td id="quantidade">{{ car.quantidade }}</td>
                 <td>
-                  <button @click="openSellModal(car)">Vender</button>
+                  <button @click="openSellModal(car)" class="button_vender">Vender</button>
                 </td>
               </tr>
             </tbody>
@@ -36,28 +36,30 @@
 
     <!-- MODAL DE VENDA -->
     <div id="sellModal" class="modal" :class="{ 'show-modal': showSellModal }">
-  <div class="modal-content">
+  <div class="modal-content" id="modalvenda">
     <span class="close" @click="closeSellModal">&times;</span>
     <h2>Venda de Veículo</h2>
     <form>
-      <label for="buyerName">Nome do Comprador:</label>
-      <input type="text" id="buyerName" v-model="buyerName" required>
-      <label for="salePrice">Preço da Venda:</label>
-      <input type="number" id="salePrice" v-model="salePrice" required>
+      <label for="model">Modelo:</label>
+      <input type="text" id="model" v-model="selectedCar.modelo" readonly>
+      <label for="price">Preço:</label>
+      <input type="text" id="price" v-model="selectedCar.preco" readonly>
       <label for="clientSelect">Cliente:</label>
       <select id="clientSelect" v-model="selectedClient">
         <option value="">Selecione um cliente</option>
-        <option v-for="client in clients" :value="client.id">{{ client.nome }}</option>
+        <option v-for="client in clients" :value="client.id">{{ client.clientes }}</option>
       </select>
       <label for="dealerSelect">Concessionária:</label>
       <select id="dealerSelect" v-model="selectedDealer">
         <option value="">Selecione uma concessionária</option>
-        <option v-for="dealer in dealers" :value="dealer.id">{{ dealer.nome }}</option>
+        <option v-for="dealer in dealers" :value="dealer.id">{{ dealer.concessionaria }}</option>
       </select>
       <button type="submit" @click.prevent="sellCar">Confirmar Venda</button>
     </form>
   </div>
 </div>
+
+
 
   </div>
 </template>
@@ -110,10 +112,10 @@
 }
 
 .modal-content {
-  background-color: #fefefe;
+  background-color: #ffffff;
   margin: 15% auto;
   padding: 20px;
-  border: 1px solid #888;
+  border: 1px solid #707070;
   width: 80%;
   max-width: 600px;
   color: #000000;
@@ -145,22 +147,88 @@ table {
   width: 100%;
   border-collapse: collapse;
   margin-top: 20px;
+
 }
 
 thead {
   background-color: #f5f5f5;
-}
 
+}
+.modal-content{
+  border-radius: 20px;
+  background-color: white;
+}
+#modalvenda{
+  width: 500px;
+}
+/*Estilização do modal vendas, inputs e textos*/
+p{
+  text-align: center;
+  font-size: 1.5rem;
+  margin-top: 50px;
+}
+h2{
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
 th {
   padding: 12px 15px;
   text-align: left;
   font-weight: bold;
   color: #000000;
-  border-bottom: 1px solid #dddddd;
+  border-bottom: 1px solid #707070;
+  font-size: 1.1rem;
+  text-align: center;
 }
 
+td{
+  font-size: 1rem;
+  text-align: center;
+}
+#model{
+  border: none;
+  outline: none;
+  background: none;
+  border-bottom: 1px solid #000000;
+  padding: 0;
+  position:relative;
+  font-size: 1rem;
+}
+label{
+  font-size: 1.1rem;
+}
+#price{
+  border: none;
+  outline: none;
+  background: none;
+  border-bottom: 1px solid #000000;
+  padding: 0;
+  position:relative;
+  font-size: 1rem;
+}
+#clientSelect{
+  border: none;
+  outline: none;
+  background: none;
+  border-bottom: 1px solid #000000;
+  padding: 0;
+  position:relative;
+  font-size: 1rem;
+}
+#dealerSelect{
+  border: none;
+  outline: none;
+  background: none;
+  border-bottom: 1px solid #000000;
+  padding: 0;
+  position:relative;
+  font-size: 1rem;
+}
+/*Estilização do modal vendas, inputs e textos FIM*/
+
 tbody tr:nth-child(even) {
-  background-color: #f9f9f9;
+  background-color: #ffffff;
 }
 
 td {
@@ -168,7 +236,22 @@ td {
   color: #000000;
   border-bottom: 1px solid #dddddd;
 }
-
+.button_vender{
+  display: inline-block;
+  width: 100%;
+  height: 43px;
+  background-color: #7814FF;
+  color: #ffffff;
+  border: none;
+  cursor: pointer;
+  border-radius: 0.2rem;
+  font-size: 0.8rem;
+  transition: 2s;
+}
+.button_vender:hover{
+  color: #ffffff;
+  background-color: #000000;
+}
 /* Estilização do modal de venda */
 #sellModal form {
   display: flex;
@@ -186,14 +269,16 @@ td {
 
 #sellModal button {
   padding: 10px;
-  background-color: #4caf50;
+  background-color: #7814FF;
   color: white;
   border: none;
   cursor: pointer;
+  transition: 2s;
+  border-radius: 10px;
 }
 
 #sellModal button:hover {
-  background-color: #45a049;
+  background-color: #000000;
 }
 
 </style>
@@ -209,22 +294,67 @@ export default {
       carData: [],
       currentPatio: {},
       patios: [],
+      selectedClient: null,
+      selectedDealer: null,
+      clients: [],
+      dealers: [],
+      selectedCar: '',
+      
     };
   },
   mounted() {
     this.fetchPatios();
+    this.fetchClients();
+    this.fetchDealers();
   },
   methods: {
     fetchPatios() {
-      fetch('http://localhost:3000/api/alocacao')
-        .then(response => response.json())
-        .then(data => {
-          this.patios = data;
+  const patios = [];
+
+  for (let numero = 1; numero <= 10; numero++) {
+    const patio = {
+      area: numero,
+      cor: ''
+    };
+
+    if (numero === 3 || numero === 5 || numero === 6) {
+      patio.cor = 'branco';
+      patios.push(patio);
+    } else {
+      this.getPatioQuantityFromDatabase(numero)
+        .then(quantidade => {
+          patio.cor = quantidade > 0 ? 'azul' : 'branco';
+          patios.push(patio);
+
+          if (patios.length === 10) {
+            patios.sort((a, b) => a.area - b.area); // Ordena os pátios pela área
+            this.patios = patios;
+          }
         })
         .catch(error => {
-          console.error('Erro ao obter os dados dos patios:', error);
+          console.error(`Erro ao obter dados do pátio ${numero}:`, error);
+          patio.cor = 'azul'; // Defina a cor como 'azul' por padrão em caso de erro
+          patios.push(patio);
+
+          if (patios.length === 10) {
+            patios.sort((a, b) => a.area - b.area); // Ordena os pátios pela área
+            this.patios = patios;
+          }
         });
-    },
+    }
+  }
+},
+getPatioQuantityFromDatabase(numero) {
+  return new Promise((resolve, reject) => {
+    fetch(`http://localhost:3000/api/patios/${numero}`)
+      .then(response => response.json())
+      .then(data => resolve(data.quantidade))
+      .catch(error => reject(error));
+  });
+},
+
+
+
     showCarInformation(patio) {
       this.currentPatio = patio;
       const area = patio.area;
@@ -239,32 +369,78 @@ export default {
           console.error('Erro ao obter os dados dos veículos:', error);
         });
     },
+
+    fetchClients() {
+    fetch('http://localhost:3000/api/clientes')
+      .then(response => response.json())
+      .then(data => {
+        this.clients = data;
+      })
+      .catch(error => {
+        console.error('Erro ao obter os dados dos clientes:', error);
+      });
+  },
+  fetchDealers() {
+    fetch('http://localhost:3000/api/concessionaria')
+      .then(response => response.json())
+      .then(data => {
+        this.dealers = data;
+      })
+      .catch(error => {
+        console.error('Erro ao obter os dados das concessionárias:', error);
+      });
+  },
     closeModal() {
       this.showModal = false;
       this.carData = [];
     },
     openSellModal(car) {
-      this.selectedCar = car;
-      this.showSellModal = true;
-    },
+  this.selectedCar = Object.assign({}, car);
+  this.showSellModal = true;
+},
+
+
     closeSellModal() {
       this.showSellModal = false;
       this.buyerName = '';
       this.salePrice = '';
     },
     sellCar() {
-      const { modelo, preco, quantidade } = this.selectedCar;
-      const sale = {
-        modelo,
-        preco,
-        quantidade,
-        comprador: this.buyerName,
-        precoVenda: this.salePrice,
-      };
-      // Aqui você pode fazer a lógica para enviar os dados da venda para o backend
-      console.log('Venda efetuada:', sale);
       this.closeSellModal();
+      if (this.selectedClient && this.selectedDealer) {
+        Swal.fire(
+          'Vendido',
+          'O veículo foi vendido com sucesso',
+          'success'
+        ).then(() => {
+          const automovelId = this.selectedCar.id;
+
+          fetch(`http://localhost:3000/api/vendas/${automovelId}`, {
+            method: 'PUT'
+          })
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              // Atualizar a quantidade do veículo no frontend
+              const carIndex = this.carData.findIndex(car => car.id === automovelId);
+              if (carIndex !== -1) {
+                this.carData[carIndex].quantidade -= 1;
+              }
+            })
+            .catch(error => {
+              console.error("Erro ao atualizar a quantidade do automóvel:", error);
+            });
+        });
+      } else {
+        Swal.fire(
+          'Erro!',
+          'Selecione um cliente e uma concessionária antes de confirmar a venda',
+          'error'
+        );
+      }
     },
+
+
   },
 };
 </script>
